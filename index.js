544f681df0491f0c3fce24055137485fa8ac5ff2
sm <a0086845@u.nus.edu>
2017-10-17 16:58:45 +0800

    mongodb setup and template pages

---
+++ index.js
@@ -0,0 +1,96 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+const bodyParser = require("body-parser")
+const path = require("path")
+const app = express()
+const User = require("./models/user")
+const Thread = require("./models/thread")
+
+//initialize mongoose and mongodb
+const mongoose = require("mongoose")
+mongoose.connect("mongodb://localhost:27017/askiez", {
+  useMongoClient: true
+})
+mongoose.Promise = global.Promise
+
+//middlewares
+app.engine("handlebars", exphbs({ defaultLayout: "main" }))
+app.set("view engine", "handlebars")
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+)
+
+//express path for views
+app.use(express.static(path.join(__dirname, "views")))
+
+//page renders
+//main page with questions
+app.get("/", (req, res) => {
+  Thread.find((err, data) => {
+    if (err) console.log(err)
+    res.render("index", { title: "Askiez", threads: data })
+  })
+})
+
+// app.get("/threads", (req, res) => {
+//   res.send(Threads.title)
+// })
+
+app.get("/thread/:idx", (req, res) => {
+  // get animals
+  let thread = Thread.find({})
+
+  //get array index from url parameter
+  let threadIndex = req.params.idx
+
+  //render page with data of the specified thread
+  Thread.findById(threadIndex, (err, data) => {
+    let currenThread = data
+    console.log(currenThread)
+    res.render("showThread", {
+      title: "thread",
+      currenThread: currenThread
+    })
+  })
+})
+
+app.post("/", (req, res) => {
+  //
+})
+
+// login page
+app.get("/login", (req, res) => {
+  res.render("login", { title: "Login" })
+})
+
+app.post("/login", (req, res) => {
+  let newUser = new User(req.body)
+  newUser.save(err => {
+    if (err) console.log(err)
+    return
+  })
+  console.log("user added")
+})
+
+//submit a question page
+app.get("/thread", (req, res) => {
+  res.render("thread", { title: "Submit a Thread" })
+})
+
+app.post("/thread", (req, res) => {
+  let newThread = new Thread(req.body)
+  newThread.save(err => {
+    if (err) console.log(err)
+    return
+  })
+  console.log(newThread)
+})
+
+//listening port
+app.listen(3000, () => {
+  console.log(`server is running on port 3000`)
+})
---
+++ models/thread.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+//answer schema
+const answerSchema = new Schema({
+  content: { type: String, required: true },
+  user: { type: String, default: "anonymous" },
+  upvotes: { type: Number, default: 0 },
+  downvotes: { type: Number, default: 0 }
+})
+
+//thread schema
+const threadSchema = new Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  user: { type: String, default: "anonymous" },
+  upvotes: { type: Number, default: 0 },
+  downvotes: { type: Number, default: 0 },
+  answers: [answerSchema]
+})
+
+const Thread = mongoose.model("thread", threadSchema)
+
+module.exports = Thread
